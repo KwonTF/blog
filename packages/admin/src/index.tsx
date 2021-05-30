b89421c973bc9e720e88da220f13599e67f1b6dc
KwonTF <ktf1008@naver.com>
2021-05-30 17:18:27 +0900

    Create React Dependencies

---
+++ packages/admin/src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+	return (
+		<div className='container'>
+			<h3 className='title'>webpack example</h3>
+		</div>
+	)
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
