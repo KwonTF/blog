3719cb421dc3d007dfa8ce7d57d08406435c4ec8
KwonTF <ktf1008@naver.com>
2021-08-22 21:59:06 +0900

    Bundle With Middlewares

--- packages/admin/src/server/index.dev.ts
+++ packages/admin/src/server/index.dev.ts
@@ -13,11 +13,12 @@ const ssrServerConfig = getSSRServerWebpackConfig({
   tsConfigPath: 'tsconfig.json',
   entry: [path.join(context, 'src/server/middlewares/local.ts')]
 })
+
 async function startServer() {
   const koaAppMiddleware = await buildHmrApp({clientConfig: clientWebpackConfig, ssrServerConfig: ssrServerConfig})
   const server = await createServer({
     port: 7650,
-    middlewares: [koaAppMiddleware],
+    middlewares: koaAppMiddleware,
     config: {
       // TODO: Apply Domain
       baseUrl: 'simpleURL',
--- packages/shared/server/middlewares/buildHmrApp.ts
+++ packages/shared/server/middlewares/buildHmrApp.ts
@@ -1,6 +1,8 @@
+import path from 'path'
+import {Middleware} from 'koa'
 import webpack, {Configuration} from 'webpack'
 import koaWebpack from 'koa-webpack'
-import {Middleware} from 'koa'
+import webpackHotMiddleware from 'koa-webpack-hot-middleware'
 
 type buildAppArgs = {
   clientConfig: Configuration
@@ -8,8 +10,11 @@ type buildAppArgs = {
 }
 
 let clientMiddleWare: Middleware
-export default async function buildHmrApp({clientConfig, ssrServerConfig}: buildAppArgs): Promise<Middleware> {
+export default async function buildHmrApp({clientConfig, ssrServerConfig}: buildAppArgs): Promise<Middleware[]> {
+  let serverModule, serverMiddleware
   const clientCompiler = webpack(clientConfig)
+  const serverCompiler = webpack(ssrServerConfig)
+
   clientMiddleWare = await koaWebpack({
     compiler: clientCompiler,
     devMiddleware: {
@@ -20,5 +25,33 @@ export default async function buildHmrApp({clientConfig, ssrServerConfig}: build
     }
   })
 
-  return clientMiddleWare
+  function passBundles() {
+    if (serverModule) {
+      serverModule.default().then((middleware) => {
+        serverMiddleware = middleware
+      })
+    }
+  }
+
+  function localWebpackMiddleware(ctx, next) {
+    if (serverMiddleware) {
+      return serverMiddleware(ctx, next)
+    }
+    return next()
+  }
+
+  serverCompiler.hooks.done.tap('serverBundle', (serverStats) => {
+    try {
+      const {compilation} = serverStats
+      const {path: outputPath} = compilation.outputOptions
+      const filename = Object.keys(compilation.assets).find((key) => /.js$/i.test(key) && key.startsWith('index'))
+      const serverOutputPath = path.join(outputPath, filename)
+
+      serverModule = require(serverOutputPath)
+      passBundles()
+    } catch (e) {
+      return
+    }
+  })
+  return [clientMiddleWare, webpackHotMiddleware(clientCompiler), localWebpackMiddleware]
 }
