4e9a1c90f53c77809fce2407b03d6c105a2981c7
KwonTF <ktf1008@naver.com>
2021-10-05 10:05:45 +0900

    Apply StyleLoader

--- packages/admin/src/server/render/app.tsx
+++ packages/admin/src/server/render/app.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import ReactDOMServer from 'react-dom/server'
 import {Context} from 'koa'
+import StyleContext from 'isomorphic-style-loader/StyleContext'
 
 import logger from '@blog/shared/utils/logger'
 
@@ -9,16 +10,31 @@ type SSRProps = {
   ctx: Context
 }
 
-export async function renderApp({ctx}: SSRProps): Promise<string> {
+type RenderAppRetrunType = {
+  renderedString: string
+  css: any
+}
+
+export async function renderApp({ctx}: SSRProps): Promise<RenderAppRetrunType> {
   let renderedString: string
   const {App} = ctx.state
   ctx.state.routerContext = {}
+
+  // CSS for all rendered React components
+  const css = new Set()
+  // eslint-disable-next-line no-underscore-dangle
+  const insertCss = (...styles) => styles.forEach((style) => css.add(style._getCss()))
+
   try {
-    renderedString = await ReactDOMServer.renderToString(<App />)
+    renderedString = await ReactDOMServer.renderToString(
+      <StyleContext.Provider value={{insertCss}}>
+        <App />
+      </StyleContext.Provider>
+    )
   } catch (error) {
     logger.log(error)
     return null
   }
 
-  return renderedString
+  return {renderedString, css}
 }
