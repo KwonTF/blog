bf4591a6837130a68a9e831f12aadc5ba425c6dd
KwonTF <ktf1008@naver.com>
2022-01-15 20:18:43 +0900

    Yarn Add Editor Build Files

--- packages/admin/src/components/pages/NewPage/NewPage.tsx
+++ packages/admin/src/components/pages/NewPage/NewPage.tsx
@@ -1,32 +1,29 @@
-import React, {FC} from 'react'
-import {CKEditor} from '@ckeditor/ckeditor5-react'
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
+import React, {FC, useRef, useState, useEffect} from 'react'
 import useStyles from 'isomorphic-style-loader/useStyles'
 
 import styles from './NewPage.scss'
 
 const NewPage: FC = () => {
   useStyles(styles)
-  return (
-    <CKEditor
-      editor={ClassicEditor}
-      data='<p>Hello from CKEditor 5!</p>'
-      onReady={(editor) => {
-        // You can store the "editor" and use when it is needed.
-        console.log('Editor is ready to use!', editor)
-      }}
-      onChange={(event, editor) => {
-        const data = editor.getData()
-        console.log({event, editor, data})
-      }}
-      onBlur={(event, editor) => {
-        console.log('Blur.', editor)
-      }}
-      onFocus={(event, editor) => {
-        console.log('Focus.', editor)
-      }}
-    />
-  )
+
+  const editorRef = useRef<any>()
+  const [editorLoaded, setEditorLoaded] = useState(false)
+  const {CKEditor, DocumentEditor} = editorRef.current || {}
+
+  useEffect(() => {
+    editorRef.current = {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      CKEditor: require('@ckeditor/ckeditor5-react')?.CKEditor,
+      DocumentEditor: require('ckeditor5-custom-build/build/ckeditor')
+    }
+    setEditorLoaded(true)
+  }, [])
+
+  if (typeof window === 'undefined') {
+    return null
+  }
+
+  return editorLoaded && <CKEditor editor={DocumentEditor} data='<p>Hello From CKEDITOR</p>' />
 }
 
 export default NewPage
