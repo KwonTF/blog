ed0a8e1d608bb6da75318c2cd82ab59f7afb9401
KwonTF <ktf1008@naver.com>
2022-07-31 16:43:47 +0900

    Connect To AWS S3 Bucket

--- packages/api/src/nest/rest/image/image.controller.ts
+++ packages/api/src/nest/rest/image/image.controller.ts
@@ -2,8 +2,17 @@ import {Get, Post, UseInterceptors, Controller, UploadedFiles} from '@nestjs/com
 import {InjectModel} from '@nestjs/mongoose'
 import {Model} from 'mongoose'
 import {FilesInterceptor} from '@nestjs/platform-express'
+import * as AWS from 'aws-sdk'
+
+import {getDecryptedData} from '@blog/shared-utils/encrypt'
+import {ServerConfig} from '@blog/shared/config'
 
 import {Grades, GradesDocument} from '@blog/api/src/schema'
+
+const ACC_KEY = getDecryptedData(ServerConfig.encrypted.s3.accessKey)
+const SECURE_ACC_KEY = getDecryptedData(ServerConfig.encrypted.s3.secureAccessKey)
+const BUCKET_NAME = getDecryptedData(ServerConfig.encrypted.s3.bucketName)
+const S3_REGION = getDecryptedData(ServerConfig.encrypted.s3.region)
 
 @Controller('image')
 export class ImageController {
@@ -18,7 +27,30 @@ export class ImageController {
   @Post()
   @UseInterceptors(FilesInterceptor('files'))
   async postImage(@UploadedFiles() files: Array<Express.Multer.File>) {
-    // eslint-disable-next-line no-console
-    console.log(files)
+    AWS.config.update({
+      credentials: {
+        accessKeyId: ACC_KEY,
+        secretAccessKey: SECURE_ACC_KEY
+      }
+    })
+
+    try {
+      const S3Service = new AWS.S3()
+      for (const file of files) {
+        const fileKey = `${Date.now() + file.originalname}`
+        await S3Service.putObject({
+          Key: fileKey,
+          Body: file.buffer,
+          Bucket: BUCKET_NAME
+        }).promise()
+
+        const signedUrl = `https://${BUCKET_NAME}.${S3_REGION}.amazonaws.com/${fileKey}`
+        // eslint-disable-next-line no-console
+        console.log(signedUrl)
+      }
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.log(error)
+    }
   }
 }
--- packages/shared/config/environment/index.ts
+++ packages/shared/config/environment/index.ts
@@ -3,6 +3,12 @@ export const ServerConfig = {
   encrypted: {
     mongoUrl:
       'bBwbf3kPmjpObNvbaJaDG60MR7HnLEA6Wfl1KnnXf1VdaI9bIjLkMAPtXQmbQZVZ82z5E4oWa3s9SNIUqQXhgwcy9nHY2wBwSmbf00NAid+vQguAlH1jLgEU85WdrGpr7ewnfTesDOJP9yKK+FCCKrUwdQ==',
-    tinyMcp: 'tLbn8Vr5x+C7hLnQADJGWEccdWO4hOM3cek+V554zElKrFNjf144kGU4YIue10Sqv7L4EPSw1oryELK34EY19Wj15+xzdAPo9Vslng=='
+    tinyMcp: 'tLbn8Vr5x+C7hLnQADJGWEccdWO4hOM3cek+V554zElKrFNjf144kGU4YIue10Sqv7L4EPSw1oryELK34EY19Wj15+xzdAPo9Vslng==',
+    s3: {
+      accessKey: 'KaZazRKEK40JI9UTE0oaWefRVIpYa7Czd/zK9Eho74Udma1d9tZ22j3Su5t6cwU/',
+      secureAccessKey: 'CP931JZW7/ZrE/YC88ZsZNCG+goVzVdW7KUgQNQk6i8fNXm8Qs5Xe884OtWCEhfTRi70/1EoQTf/gZdj9wGgy7PKlCE=',
+      bucketName: 'C+0Y86C2UiXYk1acKpMJl621o992tr9jwMT6bD4klIFKsuI05hcz+NU=',
+      region: 'Mma1+q7r/CKgsIFSyxStX7kg2BAhW2gplMJWdKAehKI2BoTvMnrLMvpNYA2a'
+    }
   }
 }
