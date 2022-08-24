8cd22ef88f92d922886193343761901585e17473
KwonTF <ktf1008@naver.com>
2022-08-24 19:53:17 +0900

    Create Post Model

--- packages/api/src/schema/index.ts
+++ packages/api/src/schema/index.ts
@@ -2,10 +2,14 @@ import {Module} from '@nestjs/common'
 import {MongooseModule} from '@nestjs/mongoose'
 
 import {Grades, GradesSchema} from './grades.schema'
+import {Posts, PostsSchema} from './posts.schema'
 
 export * from './grades.schema'
 
-const MongooseSchemas = MongooseModule.forFeature([{name: Grades.name, schema: GradesSchema}])
+const MongooseSchemas = MongooseModule.forFeature([
+  {name: Grades.name, schema: GradesSchema},
+  {name: Posts.name, schema: PostsSchema}
+])
 
 @Module({
   imports: [MongooseSchemas],
---
+++ packages/api/src/schema/posts.schema.ts
@@ -0,0 +1,51 @@
+import {Prop, Schema, SchemaFactory} from '@nestjs/mongoose'
+import {Date, Document, SchemaTypes, Types} from 'mongoose'
+
+export type PostsDocument = Posts & Document
+
+@Schema()
+export class Posts {
+  @Prop(SchemaTypes.ObjectId)
+  author: Types.ObjectId
+
+  @Prop(String)
+  thumbnail: string
+
+  @Prop(String)
+  title: string
+
+  @Prop(String)
+  body: string
+
+  @Prop(SchemaTypes.ObjectId)
+  backPost: Types.ObjectId
+
+  @Prop(SchemaTypes.ObjectId)
+  nextPost: Types.ObjectId
+
+  @Prop(SchemaTypes.ObjectId)
+  group?: Types.ObjectId
+
+  @Prop(SchemaTypes.Date)
+  createdDate: Date
+
+  @Prop(SchemaTypes.Date)
+  editedDate: Date
+
+  @Prop(Number)
+  viewCount: number
+
+  @Prop([String])
+  tags: string[]
+
+  @Prop(Number)
+  difficulty: number
+
+  @Prop({type: [{desc: {type: String}, cards: {type: {url: {type: String}, desc: {type: String}, color: {type: String}, textColor: {type: String}}}}]})
+  flickItems: {desc: string; cards: {url: string; desc: string; color: string; textColor: string}}[]
+
+  @Prop({type: [{url: {type: String}, desc: {type: String}, color: {type: String}, textColor: {type: String}}]})
+  cards: {url: string; desc: string; color: string; textColor: string}[]
+}
+
+export const PostsSchema = SchemaFactory.createForClass(Posts)
